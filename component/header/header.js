d93032d31312f5ef5620e9ba6bb0745abb3346a4
Hoo <adobephotoshop@naver.com>
2023-04-18 01:03:15 +0900

    [Header] Search 드로우바?내려오는창? 만듦

--- component/header/header.js
+++ component/header/header.js
@@ -1,6 +1,10 @@
 class Header extends HTMLElement{
-    #_logoSrc;
+    #_logoSrc
+    #_searchBarDiv
+    #_searchBar
     #_textMenuList
+    #_recommendTags
+    
 
     constructor(){
         super();
@@ -63,15 +67,58 @@ class Header extends HTMLElement{
                 ]
             }
         ];
+
+        this._recommendTags = [
+            {
+                name:"#9부팬츠",
+                link:"/"
+            },
+            {
+                name:"#자체제작",
+                link:"/"
+            },
+            {
+                name:"#맨투맨",
+                link:"/"
+            }
+        ];
     }
+
     connectedCallback(){
+        
         let headLink = document.createElement("link");
         let head = document.getElementsByTagName("head")[0];
         headLink.rel = "stylesheet";
         headLink.type = "text/css";
         headLink.href = "../component/header/header.css";
         head.appendChild(headLink);
+        //<header>
+        // <div id="searchBarDiv" style="display: block; overflow: hidden;">
+        //         <div class="SearchExitBtn"><span>✖</span></div>     
+        //         <div id="searchBar" style="top:0; overflow: hidden;">
+        //         <div class="searchLeftDiv">
+        //             <h2>검색어 입력 후 엔터</h2>
+        //             <div class="searchInnerDiv borderBottom2">
+        //                 <h1>#</h1>
+        //                 <form id="searchForm" name="searchForm">
+        //                     <input type="text" id="headerSearchInput" name="searchProductInput"/>
+        //                 </form>
+        //             </div>
+        //         </div>
+        //         <div class="searchRightDiv">
+        //             <h2>추천 검색어</h2>
+        //             <div class="searchInnerDiv">
+        //                 <span>#9부팬츠</span>
+        //                 <span>#자체제작</span>
+        //                 <span>#맨투맨</span>
+        //             </div>
+        //         </div>
+        //     </div>
+
+        //     <div id="searchBarBG"></div>
+        // </div>
         // <div id="headerNav">
+        //         </div>
         //         <div id="headerMenuBackground"></div>
         //         <div id="headerNavLogoHolder">
         //             <a href=""><img id="headerLogoImg" src="../resources/images/logo.png"/></a>
@@ -104,12 +151,87 @@ class Header extends HTMLElement{
         //             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" width="48"><path d="M105 841v-91h750v91H105Zm0-219v-91h750v91H105Zm0-220v-92h750v92H105Z"/></svg>
         //         </div>
         //     </div>
+        //</header>
+
         let header = document.createElement("header");
         this.appendChild(header);
+
+            this._searchBarDiv = document.createElement("div");
+            this._searchBarDiv.id = "searchBarDiv";
+
+            let SearchExitBtn = document.createElement("div");
+            SearchExitBtn.classList.add("SearchExitBtn");
+            let exitSpan = document.createElement("span");
+            exitSpan.textContent = "✖";
+            exitSpan.addEventListener("click",() => {
+                this.closeSearchBar();
+            })
+            SearchExitBtn.appendChild(exitSpan);
+
+            this._searchBar = document.createElement("div");
+            this._searchBar.id = "searchBar"
+            
+
+            let searchBarBG = document.createElement("div");
+            searchBarBG.id = "searchBarBG";
+            searchBarBG.addEventListener("click", () => {
+                this.closeSearchBar();
+            });
+
+            this._searchBarDiv.appendChild(this._searchBar);
+            this._searchBarDiv.appendChild(SearchExitBtn);
+            this._searchBarDiv.appendChild(searchBarBG);
+            header.appendChild(this._searchBarDiv);
+
+                let searchLeftDiv = document.createElement("div");
+                searchLeftDiv.classList.add("searchLeftDiv");
+                this._searchBar.appendChild(searchLeftDiv);
+                    let h2l = document.createElement("h2");
+                    h2l.textContent = "검색어 입력 후 엔터";
+                    searchLeftDiv.appendChild(h2l);
+                    let searchInnerDiv = document.createElement("div");
+                    searchInnerDiv.classList.add("searchInnerDiv");
+                    searchInnerDiv.classList.add("borderBottom2");
+                    searchLeftDiv.appendChild(searchInnerDiv);
+                        let h1s = document.createElement("h1");
+                        h1s.textContent = "#";
+                        searchInnerDiv.appendChild(h1s);
+
+                        let searchForm = document.createElement("form");
+                        searchForm.id = "searchForm";
+                        searchForm.name = "searchForm";
+                        searchInnerDiv.appendChild(searchForm);
+                            let headerSearchInput = document.createElement("input");
+                            headerSearchInput.id = "headerSearchInput";
+                            headerSearchInput.type = "text";
+                            headerSearchInput.name = "headerSearchInput";
+                            searchForm.appendChild(headerSearchInput);
+
+
+                let searchRightDiv = document.createElement("div");
+                searchRightDiv.classList.add("searchRightDiv");
+                this._searchBar.appendChild(searchRightDiv);
+                    let h2r = document.createElement("h2");
+                    h2r.textContent = "추천 검색어";
+                    searchRightDiv.appendChild(h2r);
+
+                    let searchInnerDiv2 = document.createElement("div");
+                    searchInnerDiv2.classList.add("searchInnerDiv");
+                    searchRightDiv.appendChild(searchInnerDiv2);
+                        for(let i=0; i<3; i++){
+                            let span = document.createElement("span");
+                            span.textContent = this._recommendTags[i].name;
+                            searchInnerDiv2.appendChild(span);
+                        }
+                    
+
+
 
             let headerNav = document.createElement("div");
                 headerNav.id = "headerNav";
                 header.appendChild(headerNav);
+
+                
 
                 let headerMenuBackground = document.createElement("div");
                 headerMenuBackground.id = "headerMenuBackground";
@@ -188,6 +310,9 @@ class Header extends HTMLElement{
                     headerNavIconMenu.appendChild(mallSVG);
 
                     let searchSVG = document.createElementNS("http://www.w3.org/2000/svg","svg");
+                    searchSVG.addEventListener("click" , ()=>{
+                        this.openSearchBar();
+                    });
                     searchSVG.setAttribute("width","48");
                     searchSVG.setAttribute("height","48");
                     searchSVG.setAttribute("viewBox","0 96 960 960");
@@ -211,5 +336,21 @@ class Header extends HTMLElement{
                 headerNav.appendChild(headerNavMenuBtn);
     }
     
+    openSearchBar(){
+        this._searchBarDiv.style.display = "block";
+        this._searchBar.classList.remove("closeSearchBar");
+        this._searchBar.classList.add("openSearchBar");
+        
+    }
+    closeSearchBar(){
+        this._searchBar.classList.remove("openSearchBar");
+        this._searchBar.classList.add("closeSearchBar");
+        setTimeout(() => {
+            this._searchBarDiv.style.display = "none";
+            this._searchBar.classList.remove("closeSearchBar");
+        }, 500);
+
+    }
 }
+
 customElements.define("header-component", Header);
